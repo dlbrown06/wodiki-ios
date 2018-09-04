be18eec1842ec1434e48edc8db42ef48e16bdb93
Doug Brown <douglas_l_brown@apple.com>
2018-09-04 11:09:33 -0700

    Init with Firebase Auth

---
+++ Pods/Target Support Files/Pods-wodikiUITests/Pods-wodikiUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_wodikiUITests : NSObject
+@end
+@implementation PodsDummy_Pods_wodikiUITests
+@end
--- wodiki/ViewController.swift
+++ wodiki/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseUI
 
 class ViewController: UIViewController {
 
@@ -20,6 +21,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func loginTapped(_ sender: UIButton) {
+        
+        // get the default Firebase Auth UI object
+        let authUI = FUIAuth.defaultAuthUI()
+        
+        guard authUI != nil else {
+            // log the error
+            return
+        }
+        
+        // set ourselves as the delegate
+        authUI?.delegate = self
+        
+        // get a reference to the auth UI view controller
+        let authViewController = authUI!.authViewController()
+        
+        // show it
+        present(authViewController, animated: true, completion: nil)
+    }
+    
 }
 
+extension ViewController: FUIAuthDelegate {
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+        
+        // check if there was an error
+        if error != nil {
+            // log the error
+            return;
+        }
+        
+        // authDataResult?.user.uid
+        
+        performSegue(withIdentifier: "goHome", sender: self)
+    }
+}
